25afa084ab90f4eeaa5a93165a8ce159b972d7da
Nikunj Saraf <46276213+NikunjSaraf@users.noreply.github.com>
2020-02-06 10:46:25 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,14 +5,14 @@
 export const environment = {
   production: false,
   firebase:{
-    apiKey: "AIzaSyDPGb5kVL6fqCZWRHJn2rCOpgoP2gQw7ls",
-    authDomain: "e-commerce-site-a6d00.firebaseapp.com",
-    databaseURL: "https://e-commerce-site-a6d00.firebaseio.com",
-    projectId: "e-commerce-site-a6d00",
-    storageBucket: "e-commerce-site-a6d00.appspot.com",
-    messagingSenderId: "741729955916",
-    appId: "1:741729955916:web:4d07eab4c8201ded1244e9",
-    measurementId: "G-DTGLW3G3QT"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   }
 };
 
